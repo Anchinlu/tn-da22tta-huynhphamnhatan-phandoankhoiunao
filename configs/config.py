e104ec6896b8da316b7273620b99d1faa4f5907f
Huỳnh Phạm Nhật An <dh2594182@gmail.com>
2026-05-13 10:48:30 +0700

    Update default Kaggle dataset path

--- configs/config.py
+++ configs/config.py
@@ -8,6 +8,6 @@ def get_args():
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--img_size', type=int, default=256)
     parser.add_argument('--dataset_path_local', type=str, default=r"D:\ĐeTaiTotNghiep\archive\kaggle_3m")
-    parser.add_argument('--dataset_path_kaggle', type=str, default="/kaggle/input/lgg-mri-segmentation/kaggle_3m")
+    parser.add_argument('--dataset_path_kaggle', type=str, default="/kaggle/input/datasets/anhpnht/lgg-mri-segmentation/kaggle_3m")
     parser.add_argument('--test_mode', action='store_true')
     return parser.parse_args()
