e7032396efa33b971eb36a8fd4169f88af1f5f9e
Huỳnh Phạm Nhật An <dh2594182@gmail.com>
2026-05-13 11:02:42 +0700

    Tăng số lượng epoch mặc định lên 150 để huấn luyện ổn định hơn

--- configs/config.py
+++ configs/config.py
@@ -3,7 +3,7 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument('--model', type=str, default='unet', choices=['unet', 'transunet'])
-    parser.add_argument('--epochs', type=int, default=30)
+    parser.add_argument('--epochs', type=int, default=150)
     parser.add_argument('--batch_size', type=int, default=16)
     parser.add_argument('--lr', type=float, default=1e-4)
     parser.add_argument('--img_size', type=int, default=256)
